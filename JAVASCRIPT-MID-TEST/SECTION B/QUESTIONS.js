b5870b3c2c3dfbf0bd2a53f66112e93370802fbb
whitneyJTonny <whitneyjosephin042@gmail.com>
2025-03-28 11:00:12 +0300

    Update QUESTIONS.js

--- JAVASCRIPT-MID-TEST/SECTION B/QUESTIONS.js
+++ JAVASCRIPT-MID-TEST/SECTION B/QUESTIONS.js
@@ -17,7 +17,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Button Click Event Example</title>
+    <title>Button Click Event </title>
 </head>
 <body>
 
